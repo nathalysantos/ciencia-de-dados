ba528704e2cd2074f1792c2b3c54031e48f066f1
Nathaly Santos <108280498+nathalysantos@users.noreply.github.com>
2024-11-01 12:23:22 -0300

    at

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -5,9 +5,9 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
     console.log(dados);
     const paragrafo document.createElement('p')
-     paragrafo.classList.add('graficos-container__texto')
+    paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-     console.log(paragrafo)
+    console.log(paragrafo)
 
 
 
